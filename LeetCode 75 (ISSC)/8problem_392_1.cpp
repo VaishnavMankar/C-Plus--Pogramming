412e5bda3fca2873a3a74f8e33e4dbf26a463323
VaishnavMankar <vmankar2910@gmail.com>
2025-01-09 22:03:33 +0530

    392.Is Subsequence

---
+++ LeetCode 75 (ISSC)/8problem_392_1.cpp
@@ -0,0 +1,23 @@
+//392.Is Subsequence
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+        while(i < s.length() && j < t.length()){
+            if(t[j] == s[i]){
+                i++;
+            }
+            j++;
+        }
+        if(i == s.length()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
