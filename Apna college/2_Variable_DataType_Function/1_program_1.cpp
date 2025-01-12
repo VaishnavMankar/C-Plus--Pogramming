466bf49dd97b3b8403e9bd4ea42cdc7cdfce23fd
VaishnavMankar <vmankar2910@gmail.com>
2025-01-12 19:54:05 +0530

    Basic

---
+++ Apna college/2_Variable_DataType_Function/1_program_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World" << "\n";
+    cout << "This is Vaishnav" << endl;
+    cout << "This is my first program.\n";
+    cout << "This is how we can brake the line \n in two parts.\n";
+    cout << "Vaishnav " << "Mankar" << endl; 
+    return 0;
+}
+//Boilerplate Code
---
+++ Apna college/2_Variable_DataType_Function/2_dataType_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 34;
+    char grade = 'a';
+    float PI = 3.14f; // we have to add a F or f at the end of the number otherwise it will consided it as double
+    bool isSafe = true;
+    double price = 75.334434;
+
+    cout << grade << endl;
+    cout << PI << endl;
+    cout << isSafe << endl;//true -> 1 & false -> 0
+    cout << price << endl;
+    return 0;
+}
