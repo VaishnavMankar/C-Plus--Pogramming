24cb021b4173bc6d6642f827d116306e284f25f3
VaishnavMankar <vmankar2910@gmail.com>
2025-01-13 07:46:19 +0530

    Basic

---
+++ Apna college/2_Variable_DataType_Function/3_typeCasting_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ 
+ int main(){
+    char Grade = 'A';// 65 aski value
+    int value = Grade;
+    cout << value << endl;
+
+    double price = 100.99;
+    int newPrice = price;
+    cout << newPrice << endl;
+    return 0;
+ }    
