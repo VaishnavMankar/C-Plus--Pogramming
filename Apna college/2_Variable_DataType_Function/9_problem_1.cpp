73badbe123068377c00e3e653c450bdb74779423
VaishnavMankar <vmankar2910@gmail.com>
2025-01-19 11:24:05 +0530

    Basic

---
+++ Apna college/2_Variable_DataType_Function/9_problem_1.cpp
@@ -0,0 +1,18 @@
+//sum of two numbers
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+
+    cout << "Enter a number: " << endl;
+    cin >> a;
+
+    cout << "Enter second nuber: " << endl;
+    cin >> b;
+
+    int sum = a + b;
+
+    cout << "Sun = " << sum << endl;
+    return 0;
+}
