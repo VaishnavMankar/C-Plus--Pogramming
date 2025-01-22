87187c3b1dda4888401cba0cb1dfb1cafa777fe5
VaishnavMankar <vmankar2910@gmail.com>
2025-01-22 20:35:13 +0530

    if_else

---
+++ Apna college/3_Conditional_Statements/1_if_else_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = -34;
+    if(n >= 0){
+        cout << "n is positive\n";
+    }
+    else{
+        cout << "n is negative\n";
+    }
+    return 0;
+}
