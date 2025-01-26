5bb1750086749764d92eb16049f4247a49587703
VaishnavMankar <vmankar2910@gmail.com>
2025-01-26 09:01:12 +0530

    find charater lowercase or uppercase

---
+++ Apna college/3_Conditional_Statements/2_problem_4_1.cpp
@@ -0,0 +1,17 @@
+//find charater lowercase or uppercase
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cout << "Enter a character: ";
+    cin >>  ch;
+
+    if(ch >= 'a' && ch <= 'z'){
+        cout << "lowercase\n";
+    }
+    else{
+        cout << "uppercase\n";
+    }
+    return 0;
+}
