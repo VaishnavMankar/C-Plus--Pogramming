56039581ddd35be9467e0bc94691c96765f1f1f3
VaishnavMankar <vmankar2910@gmail.com>
2025-02-01 23:52:40 +0530

    Ternary operator

---
+++ Apna college/3_Conditional_Statements/3_ternaryStatement_1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 34;
+                //      true        false
+    cout <<(n >= 0 ? "positive" : "negative") << endl;
+    return 0;
+}
