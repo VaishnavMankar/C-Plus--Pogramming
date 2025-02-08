e38c57c6833c1acc78b398070147c700e848d0ab
VaishnavMankar <vmankar2910@gmail.com>
2025-02-08 15:52:34 +0530

    Problem 1

---
+++ Apna college/3_Conditional_Statements/5_problem_1.cpp
@@ -0,0 +1,15 @@
+//sum of number from 1 to n
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    int sum = 0;
+
+    for(int i = 0; i <= n; i++){
+        sum += i;
+    }
+
+    cout << "sum = " << sum << endl;
+    return 0;
+}
