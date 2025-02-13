5ca4e7342de72dc4e0e4f33a94bb06e3b2ac9627
VaishnavMankar <vmankar2910@gmail.com>
2025-02-13 18:33:07 +0530

    Finction_1

---
+++ Apna college/5_function/1_function_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+//function definition
+void printHello(){
+    cout << "Hello\n";
+}
+
+int main(){
+  //function call / invoke
+  printHello();
+  return 0;
+}
