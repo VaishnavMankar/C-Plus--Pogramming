92c4679856c72df456940447f1d61a24cbff995c
VaishnavMankar <vmankar2910@gmail.com>
2025-02-24 08:22:22 +0530

    1732. Find the Highest Altitude

---
+++ LeetCode 75 (ISSC)/4problem_1732_2.cpp
@@ -0,0 +1,29 @@
+//1732. Find the Highest Altitude
+#include<iostream>
+#include<vector>
+#include<algorithm> // for max
+using namespace std;
+
+class Solution{
+    public:
+    int largestAltitude(vector<int>& gain){
+        int currentAltitude = 0;
+        int highestAltitude = 0;
+
+        for(int i = 0; i < gain.size(); i++){
+            currentAltitude += gain[i];
+            highestAltitude = max(highestAltitude, currentAltitude);
+        }
+        return highestAltitude;
+    }
+};
+
+int main(){
+    Solution solution;
+    //test case
+    vector<int> gain = {-5,1,5,0,-7};
+    int result = solution.largestAltitude(gain);
+    cout << "Highest altitude: " << result << endl;
+
+    return 0;
+}
