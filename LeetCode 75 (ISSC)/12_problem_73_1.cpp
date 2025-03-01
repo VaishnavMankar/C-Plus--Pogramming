8817b402d1b630375c06e700d215f6be2ab126c6
VaishnavMankar <vmankar2910@gmail.com>
2025-03-01 09:11:31 +0530

    283.Move Zeroes

---
+++ LeetCode 75 (ISSC)/12_problem_73_1.cpp
@@ -0,0 +1,47 @@
+//73. Set Matrix Zeroes
+//Class OP
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> col(n, 0);
+        vector<int> row(m, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> matrix = { {1, 1, 1}, {1, 0, 1}, {1, 1, 1} };
+    sol.setZeroes(matrix);
+    
+    cout << "Output matrix:" << endl;
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
