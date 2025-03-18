21173d051f325ab41fe13d19516f5ecc7b874326
VaishnavMankar <vmankar2910@gmail.com>
2025-03-18 08:18:17 +0530

    1512. Number of Good Pairs

---
+++ LeetCode 75 (ISSC)/34_problem_1512_1.cpp
@@ -0,0 +1,38 @@
+
+//1512. Number of Good Pairs
+// Hash Table
+//(class)
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int sz = nums.size();
+        
+        unordered_map<int ,int> us;
+
+        for(int i = 0; i < sz; i++)
+          us[nums[i]]++;
+
+        int cnt(0);
+        for(auto e: us)
+          if(e.second > 1)
+             cnt += (e.second * (e.second - 1)) / 2;
+
+        return cnt;
+    }
+};
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1, 1, 3}; // Example input
+
+    Solution sol;
+    int result = sol.numIdenticalPairs(nums);
+
+    cout << "The number of good pairs is: " << result << endl;
+
+    return 0;
+}
