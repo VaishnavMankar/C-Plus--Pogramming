e88df75a52774913dbec9506773141a4c6cfbb70
VaishnavMankar <vmankar2910@gmail.com>
2025-03-25 17:47:17 +0530

    448.Find All Numbers Disappeared In An Array

---
+++ LeetCode 75 (ISSC)/7problem_448_2.cpp
@@ -0,0 +1,21 @@
+//448.Find All Numbers Disappeared In An Array
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+       vector<int> res;
+       for(int i = 0; i < nums.size();i++){
+        int curr = abs(nums[i]);
+        // nums[curr-1] = -abs(nums(curr-1));
+       }
+       for(int i = 0;i< nums.size();i++){
+        if(nums[i] > 0){
+            res.emplace_back(i+1);
+        }
+       }
+       return res;
+    }
+};
