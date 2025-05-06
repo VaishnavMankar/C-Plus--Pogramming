ae1b1885982088f1ec42363776aaa2187f30ad50
VaishnavMankar <vmankar2910@gmail.com>
2025-05-06 08:45:33 +0530

    144. Binary Tree Postorder Traversal

---
+++ LeetCode 75 (ISSC)/problem_145_1.cpp
@@ -0,0 +1,76 @@
+//144. Binary Tree Postorder Traversal
+//Binary Tree
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    // Default constructor
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    // Constructor with a value
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    // Constructor with a value and left and right children
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(!root){
+            return ans;
+        }
+        vector<int> left = postorderTraversal(root->left);
+        vector<int> right = postorderTraversal(root->right);
+
+        ans.insert(ans.end(), left.begin(), left.end());
+        ans.insert(ans.end(), right.begin(), right.end());
+        ans.push_back(root->val);
+
+        return ans;
+    }
+};
+
+// Function to create a sample binary tree
+TreeNode* createExampleTree() {
+    return new TreeNode(1, nullptr, new TreeNode(2, new TreeNode(3), nullptr));
+}
+
+// Function to print the traversal result
+void printTraversal(const vector<int>& traversal) {
+    for (int val : traversal) {
+        cout << val << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    // Creating example test tree
+    TreeNode* exampleTree = createExampleTree();
+
+    Solution solution;
+
+    // Running postorder traversal
+    vector<int> result = solution.postorderTraversal(exampleTree);
+
+    // Printing the result
+    cout << "Postorder Traversal: ";
+    printTraversal(result);
+
+    // Cleanup dynamically allocated memory
+    delete exampleTree->right->left;
+    delete exampleTree->right;
+    delete exampleTree;
+
+    return 0;
+}
