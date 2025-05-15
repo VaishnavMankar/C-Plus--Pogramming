0b1f5653b72cbde387362944ba2d3df405516cbe
VaishnavMankar <vmankar2910@gmail.com>
2025-05-15 12:50:28 +0530

    Do While Loop

---
+++ Take you forward/12doWhileLoop_1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i = 2;
+    do{           // do while execute atlist once even if the condition is true
+        cout << "Strive" << i << endl;
+        i++;
+    }while(i <= 1);
+    cout << i << endl;
+    return 0;
+}
