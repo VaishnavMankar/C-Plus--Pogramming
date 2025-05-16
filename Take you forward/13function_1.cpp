750173364f0199e6571d591e7173053d6eafeb86
VaishnavMankar <vmankar2910@gmail.com>
2025-05-16 13:36:50 +0530

    Function

---
+++ Take you forward/13function_1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Function are set of code which perform something for you 
+//Function to modularise cose
+//Function are used to increase readability 
+//Function are use to use same code multiple time 
+//types of function
+//void -> which does not return anything
+//return
+//parameterised
+//non-parameterised
+
+void printName(){
+    cout << "Hey Striver";
+}
+int main(){
+    printName();
+    return 0;
+}
