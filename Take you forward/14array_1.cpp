8246f8c482674578a465c708c4521ce43a0b4057
VaishnavMankar <vmankar2910@gmail.com>
2025-05-24 08:39:02 +0530

    Array

---
+++ Take you forward/14array_1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+// arrays are default pass by reference so you dont have to use & 
+int main(){
+    int arr[5];
+    for(int i = 0; i < 5; i++){
+        cin >> arr[i];
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
