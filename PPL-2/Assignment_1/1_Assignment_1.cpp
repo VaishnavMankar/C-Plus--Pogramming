e83ff8e174018cedcc62e6812710b799af92ebda
VaishnavMankar <vmankat2910@gmail.com>
2025-06-03 09:43:15 +0530

    Assignment Problem 1

---
+++ PPL-2/Assignment_1/1_Assignment_1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+float celciousToFarenheit(float celcious){
+    float farenheit = (1.8*celcious) + 32;
+    return farenheit;
+}
+
+int main(){
+    float celcious = 0;
+    cout << "Enter the temprature in celcious: ";
+    cin >> celcious;
+    float farenheit = celciousToFarenheit(celcious) ;
+    cout << "The temprature you entered in Farenheit will be: " << farenheit << endl;
+    return 0;
+}
+
+
+
+//git config user.name "VaishnavMankar"
+//git config user.email "vmankat2910@gmail.com"
